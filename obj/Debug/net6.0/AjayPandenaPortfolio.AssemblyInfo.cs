9fce2d818d3ec248b037a6005eaffbe09979325e
Ajay Pandena <ajaypandena19@gmail.com>
2025-05-02 01:02:52 +0530

    commit

--- obj/Debug/net6.0/AjayPandenaPortfolio.AssemblyInfo.cs
+++ obj/Debug/net6.0/AjayPandenaPortfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AjayPandenaPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce1d0462b3b308c4023ffd8755247b433602d400")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03c0e223998aa8d8add7176823d94ce01ece1f70")]
 [assembly: System.Reflection.AssemblyProductAttribute("AjayPandenaPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AjayPandenaPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
